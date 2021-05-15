51ec8902deb03edd0748cdaa75290abe587320b6
AwrenNuit <a.rennaker@yahoo.com>
2021-05-15 16:40:17 -0500

    set up firebase & basic site layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { db } from "./firebase";
 
-function App() {
+export default function App() {
+  const [backgroundList, setBackgroundList] = useState([]);
+  const [backgroundInput, setBackgroundInput] = useState("");
+
+  useEffect(() => {
+    db.ref().once("value", (snapshot) => setBackgroundList(snapshot.val()));
+  });
+
+  const postToDatabase = (e) => {
+    e.preventDefault();
+    const newPostKey = db.ref().push().key;
+    db.ref().update({ [newPostKey]: backgroundInput });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-wrapper">
+      <h1>PRIME!</h1>
+
+      <form onSubmit={postToDatabase}>
+        <label>What was your background before Prime?</label>
+        <input
+          onChange={(e) => setBackgroundInput(e.target.value)}
+          placeholder="your background"
+          type="text"
+          value={backgroundInput}
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+      <div style={{marginTop:"2rem"}}>
+        <h2>Background List</h2>
+        <ul style={{marginLeft:"1rem"}}>
+          {Object.values(backgroundList).map((item, i) => {
+            return <li key={i}>{item}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
-
-export default App;
